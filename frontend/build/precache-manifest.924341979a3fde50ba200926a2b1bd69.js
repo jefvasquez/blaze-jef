85773cc7a8ff7108044787de4ae37a3403bd5b9a
Jeferson Vasquez <jefvasquez@icloud.com>
2020-09-04 11:11:58 -0500

    update .gitignore

---
+++ frontend/build/precache-manifest.924341979a3fde50ba200926a2b1bd69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660fc820aed851effac24c7f2735c961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03917d3a271820c0b983",
+    "url": "/static/css/2.49ac02db.chunk.css"
+  },
+  {
+    "revision": "616d04d6e5c9858f5180",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "03917d3a271820c0b983",
+    "url": "/static/js/2.c6a1e700.chunk.js"
+  },
+  {
+    "revision": "eb85fa2ff650be4fb535d1bc70e9f226",
+    "url": "/static/js/2.c6a1e700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616d04d6e5c9858f5180",
+    "url": "/static/js/main.1a9b22fb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
