adaf25d65c3526047de7ac80076d3ca0447a04f6
Jeferson Vasquez <jefvasquez@icloud.com>
2020-09-04 00:14:52 -0500

    editing package.json

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 app.use('/api/customers',require('./routes/customers'));
 
 // if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../build")));
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '../../build'));
+        res.sendFile(path.join(__dirname, '../frontend/build'));
     });
 // }
 
